3ea8dcd82f274338ac624c965fe302be654d22a2
REghZy <32274404+AngryCarrot789@users.noreply.github.com>
2023-06-05 14:05:10 +0100

    Viewport pan/zoom fully works now. Also added a "ResourcePath" class to massively simply resource usage, and also got clip slicing working. Will try and get clip drag-dropping working

---
+++ FramePFX.Core/Editor/ResourceManaging/Events/ResourceItemEventHandler.cs
@@ -0,0 +1,3 @@
+namespace FramePFX.Core.Editor.ResourceManaging.Events {
+    public delegate void ResourceEventHandler(ResourceManager manager, ResourceItem item);
+}
---
+++ FramePFX.Core/Editor/ResourceManaging/Events/ResourceRenamedEventHandler.cs
@@ -0,0 +1,3 @@
+namespace FramePFX.Core.Editor.ResourceManaging {
+    public delegate void ResourceRenamedEventHandler(ResourceManager manager, ResourceItem item, string oldId, string newId);
+}

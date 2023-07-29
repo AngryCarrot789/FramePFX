ee559a3d143ce6b9fc3d40df73a29821b923808f
REghZy <32274404+AngryCarrot789@users.noreply.github.com>
2023-07-29 19:36:43 +0100

    Added number dragger cursor locking and begin/end edit commands in order to signal view models whether a user is dragging a control, and fixed the video clip history data

---
+++ FramePFX/Views/Progression/IndeterminateProgressionWindow.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace FramePFX.Views.Progression {
+    public partial class IndeterminateProgressionWindow : BaseWindow {
+        public IndeterminateProgressionWindow() {
+            InitializeComponent();
+        }
+    }
+}
